e090421f6dd4d869a46f80420c3e07369c05b0ca
zhangzhian <zhangzhian_123@qq.com>
2018-10-16 20:28:16 +0800

    添加app的title

--- app/src/main/java/com/zza/stardust/app/ui/AppFragment.java
+++ app/src/main/java/com/zza/stardust/app/ui/AppFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import com.zza.library.base.BaseFragment;
 import com.zza.library.base.BasePresenter;
+import com.zza.library.weight.TitleLayout;
 import com.zza.stardust.R;
 import com.zza.stardust.app.adpter.AppAdapter;
 import com.zza.stardust.beam.AppInfoBean;
@@ -27,6 +28,9 @@ public class AppFragment extends BaseFragment {
     @BindView(R.id.rv_app)
     RecyclerView rvApp;
     Unbinder unbinder;
+//    @BindView(R.id.tl_title)
+    TitleLayout tlTitle;
+    Unbinder unbinder1;
 
     private AppAdapter appAdapter;
     private List<AppInfoBean> appInfoBeanList;
@@ -43,10 +47,16 @@ public class AppFragment extends BaseFragment {
 
     @Override
     protected void init(View rootView) {
-        unbinder =  ButterKnife.bind(this, rootView);
+        unbinder = ButterKnife.bind(this, rootView);
+        initView(rootView);
         initAppData();
         initRecycleView();
 
+    }
+
+    private void initView(View rootView) {
+        //tlTitle =rootView.findViewById(R.id.tl_title);
+        //tlTitle.setTitle("123");
     }
 
     private void initRecycleView() {
@@ -54,12 +64,12 @@ public class AppFragment extends BaseFragment {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         rvApp.setLayoutManager(linearLayoutManager);
-        appAdapter = new AppAdapter(appInfoBeanList,getActivity());
+        appAdapter = new AppAdapter(appInfoBeanList, getActivity());
         rvApp.setAdapter(appAdapter);
     }
 
     private void initAppData() {
-        appInfoBeanList  = new ArrayList<>();
+        appInfoBeanList = new ArrayList<>();
 
         AppInfoBean appinfo = new AppInfoBean();
         appinfo.setName("上传和下载");
@@ -71,4 +81,5 @@ public class AppFragment extends BaseFragment {
         super.onDestroyView();
         unbinder.unbind();
     }
+
 }
