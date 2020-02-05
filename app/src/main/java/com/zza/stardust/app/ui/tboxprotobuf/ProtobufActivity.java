6318940d36ab1f1703e6446d789f66b37af7744f
zhangzhian <zhangzhian_123@qq.com>
2020-02-05 20:10:19 +0800

    add protobuf to project and gen protobuf java file

--- app/src/main/java/com/zza/stardust/app/ui/tboxprotobuf/ProtobufActivity.java
+++ app/src/main/java/com/zza/stardust/app/ui/tboxprotobuf/ProtobufActivity.java
@@ -5,7 +5,19 @@ import android.os.Bundle;
 import com.zza.library.base.BasePresenter;
 import com.zza.stardust.R;
 import com.zza.stardust.base.MActivity;
-
+import com.zza.stardust.app.ui.tboxprotobuf.IVITboxProto.*;
+/**
+ * @Author: 张志安
+ * @Mail: zhangzhian_123@qq.com zhangzhian2016@gmail.com
+ * @Description:
+ * java api: https://developers.google.cn/protocol-buffers/
+ * github: https://github.com/protocolbuffers/protobuf
+ * 资料: https://developers.google.cn/protocol-buffers/
+ * @CreateDate: 2020/2/5 17:47
+ * @UpdateDate: 2020/2/5 17:47
+ * @UpdateRemark:
+ * @Version: 1.0
+ */
 public class ProtobufActivity extends MActivity {
 
     @Override
